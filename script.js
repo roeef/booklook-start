320541d22822b8fc6a4113e74630adcdc508d058
roeef <roeef@mail-wise.com>
2018-01-02 12:22:19 +0200

    INDIVIDUAL EXERCISE EXTENSION 1

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
-var fetch = function (isbn) {
+var fetch = function (isbn, title) {
+    titleSearch = `+intitle:${title}`;
     $.ajax({
         method: "GET",
-        url: `https://www.googleapis.com/books/v1/volumes?q=isbn:${isbn}`, //0439023521
+        url: `https://www.googleapis.com/books/v1/volumes?q=isbn:${isbn} ${title ? titleSearch : ""}`, //0439023521
         success: function(data) {
-            let title = data.items[0].volumeInfo.title;
-            let description = data.items[0].volumeInfo.description;
-            let authors = data.items[0].volumeInfo.authors;
-            let thumbLink = data.items[0].volumeInfo.imageLinks.thumbnail;
+            for (let i=0; data.items && i < data.items.length; i++) {
+                let title = data.items[i].volumeInfo.title;
+                let description = data.items[i].volumeInfo.description;
+                let authors = data.items[i].volumeInfo.authors;
+                let thumbLink = data.items[i].volumeInfo.imageLinks.thumbnail;
 
-            bookLookResultTemplate =
-                `<h1>${title}</h1>
-                 <p>${description}</p>
-                 <h2>Wrriten by:${authors.join()}</h2>
-                 <img src="${thumbLink}">`
-            $('.book').append(bookLookResultTemplate)
+                bookLookResultTemplate =
+                    `<h1>${title}</h1>
+                     <p>${description}</p>
+                     <h2>Wrriten by:${authors.join()}</h2>
+                     <img src="${thumbLink}">`
+                $('.book').append(bookLookResultTemplate)
+            }
         },
         error: function(jqXHR, textStatus, errorThrown) {
             console.log(textStatus);
@@ -21,4 +24,4 @@ var fetch = function (isbn) {
     });
 };
 
-$(".search-book").click(function(){fetch($('#isbn').val())} );
+$(".search-book").click(function(){fetch($('#isbn').val(),$('#title').val())} );
